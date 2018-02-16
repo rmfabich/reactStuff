0ab8b5c8fe2ee40bbb0000f2a6273ae07ffb6398
[Rebecca Fabich] <[rmfabich@gmail.com]>
2018-02-16 09:04:50 -0700

    Thursday commit

--- src/ex1.js
+++ src/ex1.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import './ex1.css';
+import React, {Component} from 'react';
 //in this file, create a react component that solves the following 
 //javascript problems in the construtor function and then sets
 //the solution in a component template as dictated
@@ -8,44 +7,42 @@ import './ex1.css';
 //until the end of class. Write the number of days in your react html.
 
 //ex 2)using the object:
-class Ex1 extends React.Component {
-  constructor() {
+class Ex1 extends Component {
+  constructor(){
     super();
-    var today=new Date();
-    var cmas=new Date(today.getFullYear(), 3, 27);
-    if (today.getMonth()==3 && today.getDate()>27) {
-      cmas.setFullYear(cmas.getFullYear()+1); 
-    }  
-    var one_day=1000*60*60*24;
-    this.daysLeft = (Math.ceil((cmas.getTime()-today.getTime())/(one_day)));
-    this.data = {
-      "menu": {
-        "id": "file",
-        "value": "File",
-        "popup": {
-          "menuitem": [
-            {"value": "New", "onclick": "CreateNewDoc()"},
-            {"value": "Open", "onclick": "OpenDoc()"},
-            {"value": "Close", "onclick": "CloseDoc()"}
-          ]
-        }  
-      }
-    } 
+    this.days = function() {
+      var now = new Date();
+      var xmas = new Date("April 27, 2018");
+      var msNow = now.getTime();
+      var msXmas = xmas.getTime();
+      var time = msXmas - msNow;
+      var time2 = time/(1000*60*60*24);
+      var time3 = Math.floor(time2);
+        return "Days to another 9 to 5: " + time3;
+    }
+this.data = {"menu": {
+  "id": "file",
+  "value": "File",
+  "popup": {
+    "menuitem": [
+      {"value": "New", "onclick": "CreateNewDoc()"},
+      {"value": "Open", "onclick": "OpenDoc()"},
+      {"value": "Close", "onclick": "CloseDoc()"}
+    ]
   }
-
+}}
+  }
   render() {
-    return  (
-      <div>
-        <p className="boldText">Number of days until Code School Ends: {this.daysLeft}</p>
-      </div>
-    )
-  }
-}
+    return <div>
+   {this.days()};
+   </div>
 
-export default Ex1
+  }
+
 
 //go through each menuitem and display the "value" in your react html
 
 //ex3) using the array.reduce function, multiply all the values in the array [1,2,9]. Display 
 //the end product in your react html (jsx)
-
+}
+export default Ex1;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import BarfTwo from "./Barf/Barf.js";
-import App from "./App.js"
-import TurtleLand from "./Barf/Barf.js";
-import Ex1 from './ex1.js';
-import Cell from './Barf/array.js';
-//import"./Barf/Barf.css"
+import './Barf/Barf.css';
+//import BarfTwo from "./Barf.js";
+import App from "./App.js";
+import Barf from "./Barf/Barf.js";
+import Ex1 from "./ex1.js";
+import App2 from "./lifecycle.js";
+import Button from "./button.js";
+import 'bootstrap/dist/css/bootstrap.css';
+import Example from "./navbar.js";
+import WeatherComponent from "./Weather.js"
+
 //Lets build yer first react component! Woo hoo!!!!!
 //At it's essence a react 'component' is simply a
 //function that has a render method that returns the following:
@@ -15,104 +19,40 @@ import Cell from './Barf/array.js';
 //  </div>
 //)
 
-class BunnyLand extends React.Component{
-    constructor(rollercoaster){
-        super();
-        this.rollercoaster = rollercoaster;
-        this.array = [0,1,2,3,4,5,6,7,8]
-    }
-
-    render(){
-        let row1=[]
-        let row2=[]
-        let row3=[]
-        for (var i = 0; i < this.array.length; i++ ) {
-            if (i <= 2) {
-                row1.push(<td><Cell integer={this.array[i]} /></td>)
-            } else if (i <= 5 ) {
-                row2.push(<td><Cell integer={this.array[i]} /></td>)
-            } else {
-                row3.push(<td><Cell integer={this.array[i]} /></td>)
-            }
-        }
-        return  (
-            <div>
-                <div className="bunnyLandText skyTurtles">
-                Thunder Fluff!!!!
-            
-                <div>      
-                    </div>
-                    <div className="blankSpace">
-                    </div>
-                <div className="bunnyLandText">
-                    Yo Dude!!!
-                    <div className="turtleLandText">
-                    Eat at Turtleland!!
-                    <table>
-                        <tr>
-                            <th>FOOD</th>
-                            <th>$$PRICE$$</th>
-                            <th>Description</th>
-                        </tr>
-                        <tr>
-                            <th>Turtle Soup</th>
-                            <th>$15.99</th>
-                            <th>"Fresh-ish"</th>
-                        </tr>
-                        <tr>
-                            <th>Turtle Shell Tacos</th>
-                            <th>$3999.99</th>
-                            <th>Fermented</th>
-                        </tr>
-                    </table>
-
-                    </div>
-                    <div>
-                    <table>
-                        Fun with Numbers :)
-                        <tr>
-                            {row1}
-                        </tr>
-                        <tr>
-                            {row2}
-                        </tr>
-                        <tr>
-                            {row3}
-                        </tr>
-                    </table>
-                    </div>
-                    
-                    
-                </div>
-                <p  classname="excitedText"><TurtleLand/></p>
-            </div>
-            <p  classname="boldText"><Ex1/></p>
-            </div>
-                )
-        }
-    }
-
-
-var fluffiness = new BunnyLand("thunder fluff");
-
-
-
-
-
-
-
 //that's it, really. So lets make one
 //step 1: build a class, call it w/e you want
 //step 2: make it a subclass of "React.Component"
 //step 3: give it a render() method
 //step4: within the render method, return an div tag. Throw some html inside of there
 //BUT IT STILL WONT WORK
+class Dogs extends Component {
+    constructor(){
+      super();
+      this.state = {dog: "Dobus"};
+    
+      console.log("constructor called");
+    }
+    render() {  
+        return (
+        <div>
+           <Example />
+           <p>What kind of dog is Dobus?</p>
+           <Barf />
+           <App />
+           <Ex1 />
+           <App2 />
+           <Button />
+           <WeatherComponent />
+        </div>
+        )
+    }
+}
 
 
 //we need to get this puppy working with the html. Below is a way to 
 //inject a component into html
-ReactDOM.render(<BunnyLand/>, document.getElementById("root"));
-//step 4: insert your class name where it says "insert class here". Find the index.html file (in ../public)
+ReactDOM.render(<Dogs/>, document.getElementById('root'));
+//step 5: insert your class name where it says "insert class here". Find the index.html file (in ../public)
 //and select an html id to attach your react app to.
 //the second arg of ReactDOM.render is your familiar document.getElementById(). Use the id you just
 //selected. 
